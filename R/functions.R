7b7744fd3fa4f940f7e2b8c486ae28d2ad9e1431
Yvonne Boadi <yvonne.boadi@emory.edu>
2026-08-18 14:27:05 -0400

    function in class practice

--- R/functions.R
+++ R/functions.R
@@ -2,10 +2,31 @@
 x <- 3
 # you'll want your function to return this number
 x^2
-square <- function(x) {n <- x)
-	...
-}
+square <- function(x) {
+	squared_val <- x * x
+	return(squared_val)
+	}
 # test it out
 square(x)
 square(53)
 53^2 # does this match?
+
+raise <- function(x, power) {
+	higher <- x ^ power
+	return (higher)
+}
+
+# test with
+raise(x = 2, power = 4)
+# should give you
+2^4
+
+raise <- function(x, power = 2 ) {
+	higher <- x ^ power
+	return (higher)
+}
+
+# test
+raise(x = 5)
+# should give you
+5^2
