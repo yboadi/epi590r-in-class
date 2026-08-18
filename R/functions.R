52be3b101fb39df5724dde7bfa26b69bb95e1298
Yvonne Boadi <yvonne.boadi@emory.edu>
2026-08-18 14:03:26 -0400

    functions in class practice

---
+++ R/functions.R
@@ -0,0 +1,11 @@
+# start out with a number to test
+x <- 3
+# you'll want your function to return this number
+x^2
+square <- function(x) {n <- x)
+	...
+}
+# test it out
+square(x)
+square(53)
+53^2 # does this match?
