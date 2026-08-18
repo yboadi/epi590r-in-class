eaadbd12c69fcfcea4146d7a9ac6e6abbe5921aa
Yvonne Boadi <yvonne.boadi@emory.edu>
2026-08-18 15:50:58 -0400

    practice

--- R/functions.R
+++ R/functions.R
@@ -30,3 +30,57 @@ raise <- function(x, power = 2 ) {
 raise(x = 5)
 # should give you
 5^2
+
+
+vec <- c(34, 68, 93, 32)
+vec
+
+sd <- function(x) {
+	n <- length (x)
+	sd_val <- sum (x - (mean(x)) ^ 2) / n -1
+	if ((vec) <= 1 ) {
+		print ("NA")
+	}
+	}
+
+new_sd <- function(x) {
+	demeaned_x <- x - mean(x)
+  squared_demeaned_x <- demeaned_x^2
+  sum_of_squares <- sum(squared_demeaned_x)
+  n_minus_1 <- length(x) - 1
+  std_dev <- sqrt (sum_of_squares / n_minus_1)
+  return(std_dev)
+}
+new_sd(2)
+
+new_sd <- function(x, na.rm = TRUE) {
+	if (na.rm) {clean_x <- na.omit(x)
+		# remove NAs
+	} else {
+		#don't remove NAs
+		new_x <- x
+	} #will have a new value of x if NAs are removed, same value if not
+
+	If (length(new_x) <= 1) {
+		return_val <- NA
+	} else {
+		#calculate the standard deviation using new x and save as return_val
+		demeaned_x <- new_x - mean(new_x)
+		squared_demeaned_x <- demeaned_x^2
+		sum_of_squares <- sum(squared_demeaned_x)
+		n_minus_1 <- length(new_x) - 1
+		std_dev <- sqrt (sum_of_squares / n_minus_1)
+}
+new_sd <- function(x, na.rm = T) {
+	demeaned_x <- x - mean(x)
+	squared_demeaned_x <- demeaned_x^2
+	sum_of_squares <- sum(squared_demeaned_x)
+	n_minus_1 <- length(x) - 1
+	std_dev <- sqrt (sum_of_squares / n_minus_1)
+	return(std_dev)
+}
+new_sd(2)
+
+if (na.rm = TRUE) {
+
+}
